b88e5f710e48ca99b5d216c46c3753bc332b20e1
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-22 11:01:01 +0600

    simple footer add done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import './App.css';
 import Home from './Pages/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+      </Routes>
+      <Footer></Footer>
         <ToastContainer />
     </div>
   );
