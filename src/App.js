76f4cca855ce770fcf5e819cec6b2ff12fa00391
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-22 10:44:53 +0600

    daisyui router react toast install & setup done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { ToastContainer } from 'react-toastify';
 import './App.css';
 import Home from './Pages/Home/Home';
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div>
       <Home></Home>
+        <ToastContainer />
     </div>
   );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>this is home page</h1>
+      <h1>This is home page</h1>
     </div>
   );
 };
