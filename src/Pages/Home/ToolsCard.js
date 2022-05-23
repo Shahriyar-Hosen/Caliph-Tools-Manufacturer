189ad322bb2a54b143153d2a71fab4f74988ed31
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-23 08:46:38 +0600

    ToolsCard rady & purchase compo add

--- src/Pages/Home/ToolsCard.js
+++ src/Pages/Home/ToolsCard.js
@@ -1,22 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ToolsCard = () => {
+const ToolsCard = ({ tools }) => {
+  const { _id, name, img, description, minOrder, quantity, price } = tools;
   return (
     <div>
       <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto">
         <figure>
-          <img
-            src="https://api.lorem.space/image/shoes?w=400&h=225"
-            alt="Shoes"
-          />
+          <img src={img} alt={name} />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn bg-gradient-to-r from-accent  to-success border-0 text-white">
+          <h2 className="card-title">{name}</h2>
+          <p>
+            {description.length > 100
+              ? description.slice(0, 100) + "..."
+              : { description }}
+          </p>
+          <p>Price: ${price}</p>
+          <p>Quantity: {quantity}</p>
+          <p>Minimum order quantity: {minOrder}</p>
+          <div className="card-actions justify-center mt-2">
+            <Link
+              to={`/purchase:${_id}`}
+              className="btn bg-gradient-to-r from-accent  to-success border-0 text-white"
+            >
               Buy Now
-            </button>
+            </Link>
           </div>
         </div>
       </div>
