6f9bbd0bf6c2c95ee06aecf44646384ec41b2763
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-23 11:33:56 +0600

    create User With Email And Password

--- src/Pages/Authentication/SignUp/Signup.js
+++ src/Pages/Authentication/SignUp/Signup.js
@@ -1,13 +1,41 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.inite";
 
 const Signup = () => {
   const [agree, setAgree] = useState(true);
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const navigate = useNavigate();
+  let location = useLocation();
+
+  let signUpError;
+  let from = location.state?.from?.pathname || "/";
+
+  useEffect(() => {
+    if (user) {
+      // navigate(from, { replace: true });
+      navigate("/");
+    }
+  }, [user, navigate, from]);
+  // || gLoading || updating
+  if (loading) {
+    // return <Loading></Loading>;
+  }
+  // || gError
+  // || gError?.message || updateError?.message
+  if (error) {
+    signUpError = <p className="text-red-500">{error?.message}</p>;
+  }
+
   const handleSignup = (event) => {
     event.preventDefault();
     const name = event.target.name.value;
     const email = event.target.email.value;
     const password = event.target.password.value;
+    createUserWithEmailAndPassword(email, password);
     console.log(name, email, password);
   };
   return (
