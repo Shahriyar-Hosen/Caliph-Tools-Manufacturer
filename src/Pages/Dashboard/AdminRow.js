3807a23083c07632344dc9ecd0e4f2770dffd096
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-24 12:35:51 +0600

    make admin compo add done

---
+++ src/Pages/Dashboard/AdminRow.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+import React from "react";
+
+const AdminRow = ({ order, index, refetch }) => {
+  const { _id, toolsName, orderQuantity, price, status, paid } = order;
+
+  const updateStatus = (id) => {
+    const updateOrderStatus = {
+      status: "Shifting",
+    };
+    axios
+      .put(`http://localhost:5000/order/${_id}`, updateOrderStatus)
+      .then((res) => {
+        if (res.status === 200) {
+          console.log("Your Profile Update successfully");
+          refetch();
+          // Navigate("/dashboard");
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const deleteOrder = (id) => {
+    // Delete / DELETE Method - delete by id
+    const proceed = window.confirm("Are you sure! Delete This orders");
+    if (proceed) {
+      // Delete Method update using id
+      const url = `http://localhost:5000/orders/${id}`;
+      const addUsers = async () => {
+        try {
+          const res = await axios.delete(url);
+          if (res.data.deletedCount > 0) {
+            console.log("delete done");
+            refetch();
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      addUsers();
+    }
+    // ----------------------------------------
+  };
+  return (
+    <tr>
+      <th>{index + 1}</th>
+      <td title={toolsName}>
+        {toolsName.length > 20 ? toolsName.slice(0, 20) + "..." : toolsName}
+      </td>
+      <td>{orderQuantity}</td>
+      <td>${price}</td>
+      <td className="text-warning font-bold">
+        {!paid ? (
+          <button
+            onClick={() => updateStatus(_id)}
+            className={`btn btn-xs ${
+              status === "Shifting"
+                ? "bg-green-500 btn-accent"
+                : "bg-secondary btn-success"
+            }  text-white border-0`}
+          >
+            {status}
+          </button>
+        ) : (
+          "Unpaid"
+        )}
+      </td>
+      <td>
+        <button
+          onClick={() => deleteOrder(_id)}
+          className="btn btn-xs btn-warning hover:text-black bg-red-400 border-0 text-white"
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default AdminRow;
---
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
+import AdminRow from "./AdminRow";
+
+const MakeAdmin = () => {
+  const {
+    data: orders,
+    isLoading,
+    refetch,
+  } = useQuery("orders", () =>
+    axios.get("http://localhost:5000/orders").then((res) => res.data)
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+  return (
+    <div className="mx-3 h-full mb-8 border-b-8 border-secondary border-double">
+      <div class="overflow-x-auto mb-5">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>quantity</th>
+              <th>price</th>
+              <th>status</th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders?.map((order, index) => (
+              <AdminRow
+                key={order._id}
+                order={order}
+                index={index}
+                refetch={refetch}
+              ></AdminRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default MakeAdmin;
