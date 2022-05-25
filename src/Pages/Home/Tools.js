06bd737607dbb0cf4b81abcf22c70f7a6e425433
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-25 11:30:03 +0600

    admin tools done

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -1,22 +1,29 @@
 import axios from "axios";
 import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import auth from "../../firebase.inite";
+import useAdmin from "../../hooks/useAdmin";
 import ToolsCard from "./ToolsCard";
 
 const Tools = () => {
   const [tools, setTools] = useState([]);
   const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+  const [admin, adminLoading] = useAdmin(user);
 
   useEffect(() => {
     const toolsDataLoad = async () => {
       try {
-        const res = await axios.get("https://glacial-falls-86656.herokuapp.com/tools",{
-          headers: {
-            authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-          },
-        });
+        const res = await axios.get(
+          "https://glacial-falls-86656.herokuapp.com/tools",
+          {
+            headers: {
+              authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+          }
+        );
         setTools(res.data);
       } catch (error) {
         if (error.response.status === 401 || error.response.status === 403) {
@@ -36,12 +43,12 @@ const Tools = () => {
         Tools
       </h1>
 
-      {tools.length === 0 ? (
+      {tools.length === 0 && adminLoading ? (
         <progress className="progress block w-1/2  mx-auto"></progress>
       ) : (
         <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center gap-5">
           {tools.slice(0, 6).map((tool) => (
-            <ToolsCard key={tool._id} tools={tool}></ToolsCard>
+            <ToolsCard key={tool._id} tools={tool} admin={admin}></ToolsCard>
           ))}
         </div>
       )}
